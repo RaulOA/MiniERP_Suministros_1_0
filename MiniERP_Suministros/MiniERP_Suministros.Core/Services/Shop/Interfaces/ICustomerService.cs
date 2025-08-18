a2082282bed57cdc04fe3da75c58aadd35d55b72
User <User@WINDEV2407EVAL>
2025-08-17 19:06:57 -0600

    Agregar archivos de proyecto.

---
+++ MiniERP_Suministros/MiniERP_Suministros.Core/Services/Shop/Interfaces/ICustomerService.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------
+// Email: quickapp@ebenmonney.com
+// Templates: www.ebenmonney.com/templates
+// (c) 2024 www.ebenmonney.com/mit-license
+// ---------------------------------------
+
+using MiniERP_Suministros.Core.Models.Shop;
+
+namespace MiniERP_Suministros.Core.Services.Shop
+{
+    public interface ICustomerService
+    {
+        IEnumerable<Customer> GetTopActiveCustomers(int count);
+        IEnumerable<Customer> GetAllCustomersData();
+    }
+}
